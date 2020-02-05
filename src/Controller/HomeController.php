a933dfdacb80b4d4280df832f202c93772a1f54b
Valentin Fauchère <valentin.fauchere@gmail.com>
2020-02-05 15:48:22 +0100

    début home et admin

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomeController extends  AbstractController
+{
+    /**
+     * @Route("/", name="wild_circus_index")
+     */
+    public function index() :Response
+{
+    return $this->render('public/home.html.twig');
+}
+
+
+}
